87dc73b45c31e4869e66910dd29172291ff131fc
Nathan Jenan <nathanj@rancher.com>
2018-08-22 09:54:18 -0700

    Creating example kontainer engine driver This is an example kontainer engine driver. It is basically just a copy of the existing google kuberentes engine driver, but serves as an example of how to create a driver and package it for use with Rancher.
    Issue:
    rancher/rancher#12698

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/rancher/kontainer-engine/drivers/gke"
+	"github.com/rancher/kontainer-engine/types"
+	"github.com/sirupsen/logrus"
+)
+
+var wg = &sync.WaitGroup{}
+
+func main() {
+	if os.Args[1] == "" {
+		panic(errors.New("no port provided"))
+	}
+
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(fmt.Errorf("argument not parsable as int: %v", err))
+	}
+
+	addr := make(chan string)
+	go types.NewServer(&gke.Driver{}, addr).ServeOrDie(fmt.Sprintf("127.0.0.1:%v", port))
+
+	logrus.Infof("gke driver 2 up and running on at %v", <-addr)
+
+	wg.Add(1)
+	wg.Wait() // wait forever, we only exit if killed by parent process
+}
